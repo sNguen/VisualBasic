72772d7276199d828bac81f1dde248bc25529fde
sNguen <s.nguen1992@gmail.com>
2024-08-22 13:53:18 +0200

    Add files via upload

---
+++ OutputAndVariables/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnVariables = New System.Windows.Forms.Button()
+        Me.btnDataTypes = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(64, 50)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(210, 102)
+        Me.btnGo.TabIndex = 0
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnVariables
+        '
+        Me.btnVariables.Location = New System.Drawing.Point(349, 50)
+        Me.btnVariables.Name = "btnVariables"
+        Me.btnVariables.Size = New System.Drawing.Size(194, 102)
+        Me.btnVariables.TabIndex = 1
+        Me.btnVariables.Text = "Variables Demonstration"
+        Me.btnVariables.UseVisualStyleBackColor = True
+        '
+        'btnDataTypes
+        '
+        Me.btnDataTypes.Location = New System.Drawing.Point(600, 50)
+        Me.btnDataTypes.Name = "btnDataTypes"
+        Me.btnDataTypes.Size = New System.Drawing.Size(183, 101)
+        Me.btnDataTypes.TabIndex = 2
+        Me.btnDataTypes.Text = "Data Types"
+        Me.btnDataTypes.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(879, 450)
+        Me.Controls.Add(Me.btnDataTypes)
+        Me.Controls.Add(Me.btnVariables)
+        Me.Controls.Add(Me.btnGo)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnVariables As Button
+    Friend WithEvents btnDataTypes As Button
+End Class
---
+++ OutputAndVariables/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+
+        MsgBox("course")
+        MsgBox("VB.NET")
+        MsgBox("programming")
+        MsgBox("to")
+        MsgBox("Welcome")
+
+        MessageBox.Show("this is another message")
+
+
+    End Sub
+
+    Private Sub btnVariables_Click(sender As Object, e As EventArgs) Handles btnVariables.Click
+
+        Dim stFirstName As String
+        Dim stLastName As String
+
+
+        stFirstName = "Sunny"
+        stLastName = "Nguen"
+        MsgBox("Hello and welcome " & stFirstName & " " & stLastName & " I hope you are well")
+
+        stFirstName = "Maria"
+        stLastName = "Nguen"
+        MsgBox("Hello and welcome " & stFirstName & " " & stLastName & " I hope you are well")
+
+
+    End Sub
+
+    Private Sub btnDataTypes_Click(sender As Object, e As EventArgs) Handles btnDataTypes.Click
+
+        Dim stMake As String
+        Dim stModel As String
+        Dim iDoors As Integer
+        Dim stColor As String
+        Dim bTaxed As Boolean
+        Dim iEngineSize As Integer
+        Dim decPrice As Decimal
+        Dim dtDateRegistered As Date
+
+
+        iDoors = 5
+        stMake = "Mercedes"
+        stModel = "CLS"
+        stColor = "Black"
+        bTaxed = True
+        iEngineSize = 5000
+        decPrice = 220.369
+        dtDateRegistered = #10/22/2023#
+
+        MsgBox("the car is" & vbNewLine & stMake & vbNewLine & stModel & vbNewLine & iDoors & vbNewLine & stColor &
+            vbNewLine & bTaxed & vbNewLine & iEngineSize & vbNewLine & decPrice & vbNewLine & dtDateRegistered)
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+
+End Class
