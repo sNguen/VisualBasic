9a4c34d4091ba2f6405630a0dea229453a47747b
sNguen <s.nguen1992@gmail.com>
2024-08-22 13:54:43 +0200

    Add files via upload

---
+++ Selection With IF Statement/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub btnGreet_Click(sender As Object, e As EventArgs) Handles btnGreet.Click
+
+        Dim stCountry As String
+        stCountry = txtCountry.Text
+        'stCountry = stCountry.ToUpper 'държавите трябва да са с главна буква, а това премахва case sensitivity.
+
+        If stCountry.ToUpper = "BULGARIA" Then
+            MsgBox("Добър ден българко")
+            MsgBox("Hello")
+            MsgBox("YOU ARE BEAUTIFULL")
+        ElseIf stCountry.ToUpper = "FRANCE" Then
+            MsgBox("Bonjour")
+            MsgBox("Comment alles-vous")
+        ElseIf stCountry.ToUpper = "JAPAN" Then
+            MsgBox("konichiwa")
+            MsgBox("sayonara")
+        Else
+            MsgBox("I hope you are well")
+            MsgBox("never mind")
+        End If
+
+        MsgBox("You entered " & stCountry)
+
+
+
+    End Sub
+End Class
