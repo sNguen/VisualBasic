420416d1e0b4290480e99de7dc2752df5f31bcfd
sNguen <s.nguen1992@gmail.com>
2024-08-22 13:51:51 +0200

    Add files via upload

---
+++ Logical And Ralational Operators 1/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtExamScore = New System.Windows.Forms.TextBox()
+        Me.lblExamScore = New System.Windows.Forms.Label()
+        Me.btnGetGrade = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtExamScore
+        '
+        Me.txtExamScore.Location = New System.Drawing.Point(434, 107)
+        Me.txtExamScore.MaxLength = 3
+        Me.txtExamScore.Name = "txtExamScore"
+        Me.txtExamScore.Size = New System.Drawing.Size(193, 27)
+        Me.txtExamScore.TabIndex = 0
+        '
+        'lblExamScore
+        '
+        Me.lblExamScore.AutoSize = True
+        Me.lblExamScore.Location = New System.Drawing.Point(205, 110)
+        Me.lblExamScore.Name = "lblExamScore"
+        Me.lblExamScore.Size = New System.Drawing.Size(193, 20)
+        Me.lblExamScore.TabIndex = 1
+        Me.lblExamScore.Text = "Enter the examination score"
+        '
+        'btnGetGrade
+        '
+        Me.btnGetGrade.Location = New System.Drawing.Point(351, 204)
+        Me.btnGetGrade.Name = "btnGetGrade"
+        Me.btnGetGrade.Size = New System.Drawing.Size(151, 79)
+        Me.btnGetGrade.TabIndex = 2
+        Me.btnGetGrade.Text = "Get Grade"
+        Me.btnGetGrade.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGetGrade)
+        Me.Controls.Add(Me.lblExamScore)
+        Me.Controls.Add(Me.txtExamScore)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtExamScore As TextBox
+    Friend WithEvents lblExamScore As Label
+    Friend WithEvents btnGetGrade As Button
+End Class
---
+++ Logical And Ralational Operators 1/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub btnGetGrade_Click(sender As Object, e As EventArgs) Handles btnGetGrade.Click
+
+        Dim iScore As Integer
+        If IsNumeric(txtExamScore.Text) = True Then
+            iScore = CInt(txtExamScore.Text)
+        Else
+            MsgBox("You must enter a number")
+            Exit Sub
+        End If
+
+        iScore = txtExamScore.Text
+
+
+        ''or
+        'If iScore < 0 Or iScore > 100 Then
+        'MsgBox("That is not a valid score. Enter value between 0 and 100")
+
+
+        ''not
+        If Not (iScore >= 0 And iScore <= 100) Then
+            MsgBox("That is not a valid score. Enter value between 0 and 100")
+
+
+        ElseIf iScore <= 20 Then
+            MsgBox("Grade F")
+        ElseIf iScore > 20 And iScore <= 30 Then
+            MsgBox("Grade E")
+        ElseIf iScore > 30 And iScore <= 40 Then
+            MsgBox("Grade D")
+        ElseIf iScore > 40 And iScore <= 50 Then
+            MsgBox("Grade C")
+        ElseIf iScore > 50 And iScore <= 70 Then
+            MsgBox("Grade B")
+        Else
+            MsgBox("Grade A")
+
+        End If
+        MsgBox("All done")
+
+
+    End Sub
+
+End Class
