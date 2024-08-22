faea85b562912bc00d5608aafa018dab13219d22
sNguen <s.nguen1992@gmail.com>
2024-08-22 13:49:45 +0200

    Add files via upload

---
+++ for/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGO = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnGO
+        '
+        Me.btnGO.Location = New System.Drawing.Point(314, 307)
+        Me.btnGO.Name = "btnGO"
+        Me.btnGO.Size = New System.Drawing.Size(130, 93)
+        Me.btnGO.TabIndex = 0
+        Me.btnGO.Text = "GO"
+        Me.btnGO.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGO)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGO As Button
+End Class
---
+++ for/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnGO_Click(sender As Object, e As EventArgs) Handles btnGO.Click
+
+
+        ''for next - iteration - повторение
+
+
+        Dim iCount As Integer
+
+        ''For iCount = 1 To 5
+        ''MsgBox("Hello " & iCount)
+        ''Beep()
+        ''reading.Thread.Sleep(3000) 'pause for 3 sec
+        ''Next
+
+
+
+
+        '' For iCount = 0 To 50 Step 5
+        '' MsgBox(iCount)
+        ''Next
+
+
+
+
+        For iCount = 50 To 0 Step -5
+            MsgBox(iCount)
+        Next
+
+
+
+    End Sub
+End Class
