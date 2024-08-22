745e1f8b698ab429db1385a743fb5d28615b4015
sNguen <s.nguen1992@gmail.com>
2024-08-22 13:47:47 +0200

    ConsoleApp

---
+++ ConsoleApp1/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.Write("I can now program!") ''sledvashtiqt red kod w konzolata se otpechatwa na syshtiq red 
+        Console.WriteLine("super that is cool")
+    End Sub
+End Module
