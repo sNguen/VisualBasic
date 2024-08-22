6090e564ab434b7c30fff5442561a50c1f248454
sNguen <s.nguen1992@gmail.com>
2024-08-22 13:48:37 +0200

    exercise Array whit Loops

---
+++ exercise Array whit Loops/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnExercise1 = New System.Windows.Forms.Button()
+        Me.btnExercise2 = New System.Windows.Forms.Button()
+        Me.btnExercise3 = New System.Windows.Forms.Button()
+        Me.btnExercise4 = New System.Windows.Forms.Button()
+        Me.btnExercise5 = New System.Windows.Forms.Button()
+        Me.btnExercise6 = New System.Windows.Forms.Button()
+        Me.btnExercise7 = New System.Windows.Forms.Button()
+        Me.btnExercise8 = New System.Windows.Forms.Button()
+        Me.lblExercise1 = New System.Windows.Forms.Label()
+        Me.lblExercise2 = New System.Windows.Forms.Label()
+        Me.lblExercise3 = New System.Windows.Forms.Label()
+        Me.lblExercise4 = New System.Windows.Forms.Label()
+        Me.lblExercise5 = New System.Windows.Forms.Label()
+        Me.lblExercise6 = New System.Windows.Forms.Label()
+        Me.lblExercise7 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblUslovie = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnExercise1
+        '
+        Me.btnExercise1.Location = New System.Drawing.Point(110, 44)
+        Me.btnExercise1.Name = "btnExercise1"
+        Me.btnExercise1.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise1.TabIndex = 0
+        Me.btnExercise1.Text = "Exercise 1"
+        Me.btnExercise1.UseVisualStyleBackColor = True
+        '
+        'btnExercise2
+        '
+        Me.btnExercise2.Location = New System.Drawing.Point(110, 110)
+        Me.btnExercise2.Name = "btnExercise2"
+        Me.btnExercise2.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise2.TabIndex = 1
+        Me.btnExercise2.Text = "Exercise 2"
+        Me.btnExercise2.UseVisualStyleBackColor = True
+        '
+        'btnExercise3
+        '
+        Me.btnExercise3.Location = New System.Drawing.Point(110, 176)
+        Me.btnExercise3.Name = "btnExercise3"
+        Me.btnExercise3.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise3.TabIndex = 2
+        Me.btnExercise3.Text = "Exercise 3"
+        Me.btnExercise3.UseVisualStyleBackColor = True
+        '
+        'btnExercise4
+        '
+        Me.btnExercise4.Location = New System.Drawing.Point(110, 242)
+        Me.btnExercise4.Name = "btnExercise4"
+        Me.btnExercise4.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise4.TabIndex = 3
+        Me.btnExercise4.Text = "Exercise 4"
+        Me.btnExercise4.UseVisualStyleBackColor = True
+        '
+        'btnExercise5
+        '
+        Me.btnExercise5.Location = New System.Drawing.Point(110, 308)
+        Me.btnExercise5.Name = "btnExercise5"
+        Me.btnExercise5.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise5.TabIndex = 4
+        Me.btnExercise5.Text = "Exerscise 5" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        Me.btnExercise5.UseVisualStyleBackColor = True
+        '
+        'btnExercise6
+        '
+        Me.btnExercise6.Location = New System.Drawing.Point(110, 374)
+        Me.btnExercise6.Name = "btnExercise6"
+        Me.btnExercise6.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise6.TabIndex = 5
+        Me.btnExercise6.Text = "Exercise 6"
+        Me.btnExercise6.UseVisualStyleBackColor = True
+        '
+        'btnExercise7
+        '
+        Me.btnExercise7.Location = New System.Drawing.Point(110, 440)
+        Me.btnExercise7.Name = "btnExercise7"
+        Me.btnExercise7.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise7.TabIndex = 6
+        Me.btnExercise7.Text = "Exercise 7"
+        Me.btnExercise7.UseVisualStyleBackColor = True
+        '
+        'btnExercise8
+        '
+        Me.btnExercise8.Location = New System.Drawing.Point(110, 506)
+        Me.btnExercise8.Name = "btnExercise8"
+        Me.btnExercise8.Size = New System.Drawing.Size(117, 46)
+        Me.btnExercise8.TabIndex = 7
+        Me.btnExercise8.Text = "Exercise 8"
+        Me.btnExercise8.UseVisualStyleBackColor = True
+        '
+        'lblExercise1
+        '
+        Me.lblExercise1.AutoSize = True
+        Me.lblExercise1.Location = New System.Drawing.Point(246, 57)
+        Me.lblExercise1.Name = "lblExercise1"
+        Me.lblExercise1.Size = New System.Drawing.Size(430, 20)
+        Me.lblExercise1.TabIndex = 9
+        Me.lblExercise1.Text = "Output each item in a separate massage boxx one after another" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblExercise2
+        '
+        Me.lblExercise2.AutoSize = True
+        Me.lblExercise2.Location = New System.Drawing.Point(246, 123)
+        Me.lblExercise2.Name = "lblExercise2"
+        Me.lblExercise2.Size = New System.Drawing.Size(440, 20)
+        Me.lblExercise2.TabIndex = 10
+        Me.lblExercise2.Text = "Output all of the items in the same massage box in separate lines"
+        '
+        'lblExercise3
+        '
+        Me.lblExercise3.AutoSize = True
+        Me.lblExercise3.Location = New System.Drawing.Point(246, 189)
+        Me.lblExercise3.Name = "lblExercise3"
+        Me.lblExercise3.Size = New System.Drawing.Size(421, 20)
+        Me.lblExercise3.TabIndex = 11
+        Me.lblExercise3.Text = "Add up all of the items then output the total in a message box" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblExercise4
+        '
+        Me.lblExercise4.AutoSize = True
+        Me.lblExercise4.Location = New System.Drawing.Point(246, 255)
+        Me.lblExercise4.Name = "lblExercise4"
+        Me.lblExercise4.Size = New System.Drawing.Size(435, 20)
+        Me.lblExercise4.TabIndex = 12
+        Me.lblExercise4.Text = "Calculate the average of the items then output in a message box"
+        '
+        'lblExercise5
+        '
+        Me.lblExercise5.AutoSize = True
+        Me.lblExercise5.Location = New System.Drawing.Point(246, 321)
+        Me.lblExercise5.Name = "lblExercise5"
+        Me.lblExercise5.Size = New System.Drawing.Size(485, 20)
+        Me.lblExercise5.TabIndex = 13
+        Me.lblExercise5.Text = "Add up the items grater than 20 then output the result in a message box"
+        '
+        'lblExercise6
+        '
+        Me.lblExercise6.AutoSize = True
+        Me.lblExercise6.Location = New System.Drawing.Point(246, 387)
+        Me.lblExercise6.Name = "lblExercise6"
+        Me.lblExercise6.Size = New System.Drawing.Size(345, 20)
+        Me.lblExercise6.TabIndex = 14
+        Me.lblExercise6.Text = "Find the largest item then output in a message box" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblExercise7
+        '
+        Me.lblExercise7.AutoSize = True
+        Me.lblExercise7.Location = New System.Drawing.Point(246, 453)
+        Me.lblExercise7.Name = "lblExercise7"
+        Me.lblExercise7.Size = New System.Drawing.Size(359, 20)
+        Me.lblExercise7.TabIndex = 15
+        Me.lblExercise7.Text = "Find the smallest item the output it in a message box"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(246, 519)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(636, 20)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Replace each item in the array with a new value that is twice as big then output " &
+    "the new values "
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(110, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(547, 41)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Coding exercises usin Arrays with Loops"
+        '
+        'lblUslovie
+        '
+        Me.lblUslovie.AutoSize = True
+        Me.lblUslovie.Location = New System.Drawing.Point(889, 96)
+        Me.lblUslovie.Name = "lblUslovie"
+        Me.lblUslovie.Size = New System.Drawing.Size(178, 140)
+        Me.lblUslovie.TabIndex = 18
+        Me.lblUslovie.Text = "Dim aiData (4) As Integer" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData (0) = 5" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData (1) = 7" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData (2) = 33" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiD" &
+    "ata (3) = 22" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "aiData (4) = 8" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1143, 588)
+        Me.Controls.Add(Me.lblUslovie)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblExercise7)
+        Me.Controls.Add(Me.lblExercise6)
+        Me.Controls.Add(Me.lblExercise5)
+        Me.Controls.Add(Me.lblExercise4)
+        Me.Controls.Add(Me.lblExercise3)
+        Me.Controls.Add(Me.lblExercise2)
+        Me.Controls.Add(Me.lblExercise1)
+        Me.Controls.Add(Me.btnExercise8)
+        Me.Controls.Add(Me.btnExercise7)
+        Me.Controls.Add(Me.btnExercise6)
+        Me.Controls.Add(Me.btnExercise5)
+        Me.Controls.Add(Me.btnExercise4)
+        Me.Controls.Add(Me.btnExercise3)
+        Me.Controls.Add(Me.btnExercise2)
+        Me.Controls.Add(Me.btnExercise1)
+        Me.Name = "Form1"
+        Me.Tag = ""
+        Me.Text = "Exercises"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnExercise1 As Button
+    Friend WithEvents btnExercise2 As Button
+    Friend WithEvents btnExercise3 As Button
+    Friend WithEvents btnExercise4 As Button
+    Friend WithEvents btnExercise5 As Button
+    Friend WithEvents btnExercise6 As Button
+    Friend WithEvents btnExercise7 As Button
+    Friend WithEvents btnExercise8 As Button
+    Friend WithEvents lblExercise1 As Label
+    Friend WithEvents lblExercise2 As Label
+    Friend WithEvents lblExercise3 As Label
+    Friend WithEvents lblExercise4 As Label
+    Friend WithEvents lblExercise5 As Label
+    Friend WithEvents lblExercise6 As Label
+    Friend WithEvents lblExercise7 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblUslovie As Label
+End Class
---
+++ exercise Array whit Loops/Form1.vb
@@ -0,0 +1,185 @@
+﻿Public Class Form1
+    Private Sub btnExercise1_Click(sender As Object, e As EventArgs) Handles btnExercise1.Click
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            MsgBox(aiData(i))
+        Next
+
+
+    End Sub
+
+    Private Sub btnExercise2_Click(sender As Object, e As EventArgs) Handles btnExercise2.Click
+        Dim aiData(4) As Integer
+        Dim stOut As String
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            stOut = stOut & aiData(i) & vbNewLine
+        Next
+        MsgBox(stOut)
+    End Sub
+
+    Private Sub btnExercise3_Click(sender As Object, e As EventArgs) Handles btnExercise3.Click
+
+        Dim aiData(4) As Integer
+        Dim iTotal As Integer
+        Dim i As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            iTotal = iTotal + aiData(i)
+            MsgBox("The total is " & iTotal)
+        Next
+
+    End Sub
+
+    Private Sub btnExercise4_Click(sender As Object, e As EventArgs) Handles btnExercise4.Click
+
+        Dim aiData(4) As Integer
+        Dim i As Integer
+        Dim iTotal As Integer
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        For i = 0 To 4
+            iTotal = iTotal + aiData(i)
+        Next
+
+        ''  iTotal = (aiData(0) + aiData(1) + aiData(2) + aiData(3) + aiData(4)) / 5   ----
+
+
+        MsgBox("The average of the items  " & iTotal / 5)
+
+    End Sub
+
+    Private Sub btnExercise5_Click(sender As Object, e As EventArgs) Handles btnExercise5.Click
+
+        Dim aiData(4) As Integer
+
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iTotal As Integer
+
+        For i = 0 To 4
+            If aiData(i) > 20 Then
+                iTotal = iTotal + aiData(i)
+            End If
+        Next
+
+        MsgBox("The total of the items bigger then 20 is " & iTotal)
+
+
+
+    End Sub
+
+    Private Sub btnExercise6_Click(sender As Object, e As EventArgs) Handles btnExercise6.Click
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iMax As Integer
+
+        iMax = 0
+
+        For i = 0 To 4
+            If aiData(i) > iMax Then
+                iMax = aiData(i)
+            End If
+
+        Next
+        MsgBox("The largest number is " & iMax)
+
+
+    End Sub
+
+    Private Sub btnExercise7_Click(sender As Object, e As EventArgs) Handles btnExercise7.Click
+
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+        Dim iMin As Integer
+
+        iMin = 1000
+
+        For i = 0 To 4
+            If aiData(i) < iMin Then
+                iMin = aiData(i)
+            End If
+
+        Next
+        MsgBox("The smallest number is " & iMin)
+
+
+    End Sub
+
+    Private Sub btnExercise8_Click(sender As Object, e As EventArgs) Handles btnExercise8.Click
+
+        Dim aiData(4) As Integer
+
+        aiData(0) = 5
+        aiData(1) = 7
+        aiData(2) = 33
+        aiData(3) = 22
+        aiData(4) = 8
+
+        Dim i As Integer
+
+        For i = 0 To 4
+            aiData(i) = aiData(i) * 2
+        Next
+
+        Dim stOut As String
+
+        For i = 0 To 4
+            stOut = stOut & aiData(i) & vbNewLine
+        Next
+
+        MsgBox(stOut)
+
+
+
+
+    End Sub
+End Class
